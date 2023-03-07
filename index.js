9d9b9de00edb49474bc75674cfa0753cf8c6b80a
git-savio1.domi <techsavvydominic3@gmail.com>
2023-03-07 13:34:38 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const input = document.getElementById('input');
-
-input.addEventListener('click', addingEventListener);
 function addingEventListener() {
-  alert('I was clicked!');
+  const input = document.getElementById('button');
+ 
+  input.addEventListener('click', function () {
+     alert('I was clicked!');
+  })
 }
