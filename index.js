3256f39d2918e39f528881178de4433a9cf08e5e
git-savio1.domi <techsavvydominic3@gmail.com>
2023-03-21 18:52:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function addingEventListener() {
+ // Returns a reference to the first object with the specified value of the id attribute(button).
   const input = document.getElementById('button');
- 
+  
+ // Appends an event listener for events whose type attribute value is type
   input.addEventListener('click', function () {
      alert('I was clicked!');
   })
